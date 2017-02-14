3c6a4313a3a6b015e242d8a34d3ce442db2a5a4b
Victor Magana <vicmagana@hotmail.com>
2017-02-13 19:08:12 -0800

    created a main file and moved smoketest to a class
    created main file to drive the testing and smoketest is a class
    cleaner way of adding test in main

---
+++ testscripts/src/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+#import paramiko
+import sys
+import os
+from logger import logger
+import logging
+#from sshdevice import sshdevice
+import ConfigParser
+import multiprocessing
+import time
+import re
+import unittest
+from smoketest import smoketest
+
+def main(params):
+        (system_ip,username,pwd,debug_to_file,threadlock)=params
+        logger(debug_to_file)
+        logfile=logging.getLogger('logmain')
+        logfile.info("*****************START LOG***************************")
+
+        run_test=smoketest(system_ip,username,pwd,debug_to_file,threadlock)
+        if run_test.test_move_files() == False:
+            return False
+        else:
+            return True
+
+
+
+if __name__ == '__main__':
+
+    #init config parser to read all the systems to be tested
+    iniparser=ConfigParser.ConfigParser()
+    iniparser.read('systems.ini')
+    number_of_systems=iniparser.getint('config','count')
+    bool_log_to_file=iniparser.getboolean('config','debug')
+
+    #create logging object first time
+    logger(bool_log_to_file)
+    logfile=logging.getLogger('logmain')
+    systems_list=[]
+    failures=0
+
+    threadpool=multiprocessing.Pool()
+    threadlock=multiprocessing.Manager().Lock()
+
+    #loop thru the systems in the ini file
+    for i in range(1,number_of_systems+1):
+        systems_list+=[(iniparser.get('s'+str(i),'ip'),iniparser.get('s'+str(i),'username'),iniparser.get('s'+str(i),'pwd'),bool_log_to_file,threadlock)]
+
+    results=threadpool.map(main, systems_list)
+    logfile.info("Done running all threads")
+    threadpool.close()
+    threadpool.join()
+
+    logfile.info("Getting exit code for threads")
+    for (ip,user,pwd,debug_value,tlock),retcode in zip(systems_list,results):
+        logfile.info('%s %s %s %s' % (retcode,ip,user,pwd))
+        if retcode == False:
+            failures+=1
+
+    if failures != 0:
+        logfile.error("There were %s failures in a thread(s), please check log" % failures)
+        sys.exit(False)
+    else:
+        logfile.info("Done running smoke test, found no failures")
+        sys.exit(True)
+
+
+
--- testscripts/src/smoketest.py
+++ testscripts/src/smoketest.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 #import paramiko
@@ -10,138 +11,111 @@ import ConfigParser
 import multiprocessing
 import time
 import re
-
-def main(params):
-        (system_ip,username,pwd,debug_to_file,threadlock)=params
-        logger(debug_to_file)
-        logfile=logging.getLogger('logmain')
-        logfile.info("*****************START LOG***************************")
-        logfile.info("PID %s" % os.getpid())
-
-        #create device object
-        dut=sshdevice(system_ip,username,pwd)
-        if dut.connect() == False:
-            return False
-
-        out=dut.send_cmd("ls -la")
-        if out == False:
-            return False
-        data=out.readlines()
-        for line in data:
-            print line.strip()
-            #sys.stdout.write(line)
-
-        out=dut.send_cmd('cd hello_project;ls -la')
-        if out == False:
-            return False
-        data=out.readlines()
-        for line in data:
-            print line.strip()
-            #sys.stdout.write(line)
-
-        #files=os.listdir(os.curdir)
-        #logfile.info(files)
-
-        threadlock.acquire()
-        logfile.info("Acquired lock")
-        if dut.put_files("test.cpp", "/root/hello_project/test.cpp") == False:
-            return False
-
-        time.sleep(2)
-
-        if dut.get_files("/root/hello_project/test.spec", "newspec.file") == False:
-            return False
-        threadlock.release()
-        logfile.info("Released lock")
-
-        threadlock.acquire()
-        logfile.info('Acquired lock for rpm installation')
-        #delete previous rpm
-        logfile.info("Deleting previous rpm")
-        if dut.send_cmd('rm -rf hello_project/rpmbuild/RPMS/i686/*') == False:
-            return False
-
-        #delete previous binary
-        logfile.info("Deleting previos binary")
-        if dut.send_cmd('rm -rf /usr/bin/helloworld') == False:
-            return False
-
-        #create new rpm
-        logfile.info("Building new rpm")
-        if dut.send_cmd('rpmbuild -ba hello_project/rpmbuild/SPECS/test.spec') == False:
-            return False
-
-        #verify rpm exist and is at the correct location
-        out=dut.send_cmd("ls hello_project/rpmbuild/RPMS/i686/")
-        if out == False:
-            return False
-        dline=out.readline()
-        rpm_match=re.match('helloworld.*i686\.rpm',dline)
-        if rpm_match == None:
-            logfile.info("No rpm match found")
-            return False
-        else:
-            rpm_name=rpm_match.group()
-            logfile.info("Match, found rpm %s " % rpm_name)
-
-        #install rpm
-        logfile.info("Installing newly built rpm")
-        if dut.send_cmd('rpm -Uvh --force hello_project/rpmbuild/RPMS/i686/' + rpm_name) == False:
-            return False
-
-        #run binary installed by rpm
-        logfile.info("Executing binary installed by rpm")
-        out=dut.send_cmd('/usr/bin/helloworld')
-        if out == False:
-            return False
-        else:
-            logfile.info('Output of binary installed from rpm \"%s\" ' % out.readline().strip())
-
-        threadlock.release()
-        logfile.info('Released lock from rpm installation')
-
-        dut.done()
-        return True
+import unittest
+from unittest import TestCase
 
 
-if __name__ == '__main__':
+class smoketest():
 
-    #init config parser to read all the systems to be tested
-    iniparser=ConfigParser.ConfigParser()
-    iniparser.read('systems.ini')
-    number_of_systems=iniparser.getint('config','count')
-    bool_log_to_file=iniparser.getboolean('config','debug')
+        def __init__(self,system_ip,username,password,debug_enabled,thread_lock):
+            self.system_ip=system_ip
+            self.username=username
+            self.pwd=password
+            self.debug_enabled=debug_enabled
+            self.thread_lock=thread_lock
 
-    #create logging object first time
-    logger(bool_log_to_file)
-    logfile=logging.getLogger('logmain')
-    systems_list=[]
-    failures=0
+        def test_move_files(self):
+            #logger(debug_enabled)
+            logfile=logging.getLogger('logmain')
+            logfile.info("*****************START SMOKETEST LOG***************************")
+            logfile.info("PID %s" % os.getpid())
 
-    threadpool=multiprocessing.Pool()
-    threadlock=multiprocessing.Manager().Lock()
+            #create device object
+            self.dut=sshdevice(self.system_ip,self.username,self.pwd)
+            if self.dut.connect() == False:
+                return False
 
-    #loop thru the systems in the ini file
-    for i in range(1,number_of_systems+1):
-        systems_list+=[(iniparser.get('s'+str(i),'ip'),iniparser.get('s'+str(i),'username'),iniparser.get('s'+str(i),'pwd'),bool_log_to_file,threadlock)]
+            self.out=self.dut.send_cmd("ls -la")
+            if self.out == False:
+                return False
+            self.data=out.readlines()
+            for self.line in self.data:
+                print self.line.strip()
+                #sys.stdout.write(line)
 
-    results=threadpool.map(main, systems_list)
-    logfile.info("Done running all threads")
-    threadpool.close()
-    threadpool.join()
+            self.out=self.dut.send_cmd('cd hello_project;ls -la')
+            if self.out == False:
+                return False
+            self.data=self.out.readlines()
+            for self.line in self.data:
+                print self.line.strip()
+                #sys.stdout.write(line)
 
-    logfile.info("Getting exit code for threads")
-    for (ip,user,pwd,debug_value,tlock),retcode in zip(systems_list,results):
-        logfile.info('%s %s %s %s' % (retcode,ip,user,pwd))
-        if retcode == False:
-            failures+=1
+            #files=os.listdir(os.curdir)
+            #logfile.info(files)
 
-    if failures != 0:
-        logfile.error("There were %s failures in a thread(s), please check log" % failures)
-        sys.exit(False)
-    else:
-        logfile.info("Done running smoke test, found no failures")
-        sys.exit(True)
+            self.threadlock.acquire()
+            self.logfile.info("Acquired lock")
+            if self.dut.put_files("test.cpp", "/root/hello_project/test.cpp") == False:
+                return False
 
+            time.sleep(2)
+
+            if self.dut.get_files("/root/hello_project/test.spec", "newspec.file") == False:
+                return False
+            self.threadlock.release()
+            self.logfile.info("Released lock")
+
+            self.threadlock.acquire()
+            self.logfile.info('Acquired lock for rpm installation')
+            #delete previous rpm
+            self.logfile.info("Deleting previous rpm")
+            if self.dut.send_cmd('rm -rf hello_project/rpmbuild/RPMS/i686/*') == False:
+                return False
+
+            #delete previous binary
+            self.logfile.info("Deleting previos binary")
+            if self.dut.send_cmd('rm -rf /usr/bin/helloworld') == False:
+                return False
+
+            #create new rpm
+            self.logfile.info("Building new rpm")
+            if self.dut.send_cmd('rpmbuild -ba hello_project/rpmbuild/SPECS/test.spec') == False:
+                return False
+
+            #verify rpm exist and is at the correct location
+            self.out=self.dut.send_cmd("ls hello_project/rpmbuild/RPMS/i686/")
+            if self.out == False:
+                return False
+            self.dline=self.out.readline()
+            self.rpm_match=re.match('helloworld.*i686\.rpm',self.dline)
+            if self.rpm_match == None:
+                self.logfile.info("No rpm match found")
+                return False
+            else:
+                self.rpm_name=rpm_match.group()
+                self.logfile.info("Match, found rpm %s " % self.rpm_name)
+
+            #install rpm
+            self.logfile.info("Installing newly built rpm")
+            if vdut.send_cmd('rpm -Uvh --force hello_project/rpmbuild/RPMS/i686/' + self.rpm_name) == False:
+                return False
+
+            #run binary installed by rpm
+            self.logfile.info("Executing binary installed by rpm")
+            self.out=self.dut.send_cmd('/usr/bin/helloworld')
+            if out == False:
+                return False
+            else:
+                self.logfile.info('Output of binary installed from rpm \"%s\" ' % out.readline().strip())
+
+            self.threadlock.release()
+            self.logfile.info('Released lock from rpm installation')
+
+            self.dut.done()
+            return True
+
+            def tearDown(self):
+                self.browser.close()
 
 
