7e77d5e5ed0485f68ee8da79ed198030a478908c
Victor Magana <vicmagana@hotmail.com>
2017-02-15 22:53:49 -0800

    missed this file
    missed this file in previous checkin

--- testscripts/src/smoketest.py
+++ testscripts/src/smoketest.py
@@ -1,7 +1,3 @@
-
-#!/usr/bin/env python
-
-#import paramiko
 import sys
 import os
 from logger import logger
@@ -14,7 +10,6 @@ import re
 import unittest
 from unittest import TestCase
 
-
 class smoketest():
 
         def __init__(self,system_ip,username,password,debug_enabled,thread_lock):
@@ -23,12 +18,13 @@ class smoketest():
             self.pwd=password
             self.debug_enabled=debug_enabled
             self.thread_lock=thread_lock
+            self.project_path="/root/hello_project/"
 
         def test_move_files(self):
-            #logger(debug_enabled)
-            logfile=logging.getLogger('logmain')
-            logfile.info("*****************START SMOKETEST LOG***************************")
-            logfile.info("PID %s" % os.getpid())
+            #self.logger(self.debug_enabled)
+            self.logfile=logging.getLogger("logmain")
+            self.logfile.info("*****************START SMOKETEST LOG***************************")
+            self.logfile.info("PID %s" % os.getpid())
 
             #create device object
             self.dut=sshdevice(self.system_ip,self.username,self.pwd)
@@ -38,12 +34,12 @@ class smoketest():
             self.out=self.dut.send_cmd("ls -la")
             if self.out == False:
                 return False
-            self.data=out.readlines()
+            self.data=self.out.readlines()
             for self.line in self.data:
                 print self.line.strip()
                 #sys.stdout.write(line)
 
-            self.out=self.dut.send_cmd('cd hello_project;ls -la')
+            self.out=self.dut.send_cmd("cd " + self.project_path + ";ls -la")
             if self.out == False:
                 return False
             self.data=self.out.readlines()
@@ -54,63 +50,70 @@ class smoketest():
             #files=os.listdir(os.curdir)
             #logfile.info(files)
 
-            self.threadlock.acquire()
+            self.thread_lock.acquire()
             self.logfile.info("Acquired lock")
-            if self.dut.put_files("test.cpp", "/root/hello_project/test.cpp") == False:
+            if self.dut.put_files("test.cpp", self.project_path + "remotetest.cpp") == False:
                 return False
 
             time.sleep(2)
 
-            if self.dut.get_files("/root/hello_project/test.spec", "newspec.file") == False:
+            if self.dut.get_files(self.project_path + "remotetest.cpp", "newlocaltest.cpp") == False:
                 return False
-            self.threadlock.release()
+
+            if self.dut.put_dirs("rpmbuild", self.project_path) == False:
+                return False
+
+            if self.dut.get_dirs(self.project_path + "rpmbuild","localbuildrpm") == False:
+                return False
+
+            self.thread_lock.release()
             self.logfile.info("Released lock")
 
-            self.threadlock.acquire()
-            self.logfile.info('Acquired lock for rpm installation')
+            self.thread_lock.acquire()
+            self.logfile.info("Acquired lock for rpm installation")
             #delete previous rpm
             self.logfile.info("Deleting previous rpm")
-            if self.dut.send_cmd('rm -rf hello_project/rpmbuild/RPMS/i686/*') == False:
+            if self.dut.send_cmd("rm -rf " + self.project_path + "rpmbuild/RPMS/i686/*") == False:
                 return False
 
             #delete previous binary
             self.logfile.info("Deleting previos binary")
-            if self.dut.send_cmd('rm -rf /usr/bin/helloworld') == False:
+            if self.dut.send_cmd("rm -rf /usr/bin/helloworld") == False:
                 return False
 
             #create new rpm
             self.logfile.info("Building new rpm")
-            if self.dut.send_cmd('rpmbuild -ba hello_project/rpmbuild/SPECS/test.spec') == False:
+            if self.dut.send_cmd("rpmbuild -ba " + self.project_path + "rpmbuild/SPECS/test.spec") == False:
                 return False
 
             #verify rpm exist and is at the correct location
-            self.out=self.dut.send_cmd("ls hello_project/rpmbuild/RPMS/i686/")
+            self.out=self.dut.send_cmd("ls " + self.project_path + "rpmbuild/RPMS/i686/")
             if self.out == False:
                 return False
             self.dline=self.out.readline()
-            self.rpm_match=re.match('helloworld.*i686\.rpm',self.dline)
+            self.rpm_match=re.match("helloworld.*i686\.rpm",self.dline)
             if self.rpm_match == None:
                 self.logfile.info("No rpm match found")
                 return False
             else:
-                self.rpm_name=rpm_match.group()
+                self.rpm_name=self.rpm_match.group()
                 self.logfile.info("Match, found rpm %s " % self.rpm_name)
 
             #install rpm
             self.logfile.info("Installing newly built rpm")
-            if vdut.send_cmd('rpm -Uvh --force hello_project/rpmbuild/RPMS/i686/' + self.rpm_name) == False:
+            if self.dut.send_cmd("rpm -Uvh --force " + self.project_path + "rpmbuild/RPMS/i686/" + self.rpm_name) == False:
                 return False
 
             #run binary installed by rpm
             self.logfile.info("Executing binary installed by rpm")
-            self.out=self.dut.send_cmd('/usr/bin/helloworld')
-            if out == False:
+            self.out=self.dut.send_cmd("/usr/bin/helloworld")
+            if self.out == False:
                 return False
             else:
-                self.logfile.info('Output of binary installed from rpm \"%s\" ' % out.readline().strip())
+                self.logfile.info("Output of binary installed from rpm \"%s\" " % self.out.readline().strip())
 
-            self.threadlock.release()
-            self.logfile.info('Released lock from rpm installation')
+            self.thread_lock.release()
+            self.logfile.info("Released lock from rpm installation")
 
             self.dut.done()
             return True
